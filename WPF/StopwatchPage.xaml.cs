2bcc303e4a59b3d2beebaa74bbe6083ee0b4e09c
KeDeX <albers.mark02@gmail.com>
2025-10-12 21:59:45 +0200

    rewrote cs and take variables from inheriated class

--- WPF/StopwatchPage.xaml.cs
+++ WPF/StopwatchPage.xaml.cs
@@ -1,64 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Clock;
+using Clock.WPF;
+using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Clock
 {
     public partial class StopwatchPage : Page
     {
-        private StopwatchTimer stopwatchTimer;
-        private bool isRunning = false;
+        private readonly StopwatchTimer stopwatchTimer;
 
         public StopwatchPage()
         {
             InitializeComponent();
+
             stopwatchTimer = new StopwatchTimer();
+
+            stopwatchTimer.Tick += (_, __) =>
+            {
+                if (stopwatchTimer.IsRunning) 
+                    TimeLabel.Content = stopwatchTimer.GetTimeString();
+            };
         }
 
         public void OnStartStopClick(object sender, RoutedEventArgs e)
         {
-            isRunning = !isRunning;
-
-            if (isRunning)
+            if (!stopwatchTimer.IsRunning)
             {
+                stopwatchTimer.Start();
                 StartStopButton.Content = "Stop";
                 StartStopButton.Background = Brushes.Red;
-                SyncTimer();
-                stopwatchTimer.StartTimer();
             }
             else
             {
+                stopwatchTimer.Stop();
                 StartStopButton.Content = "Start";
                 StartStopButton.Background = Brushes.Green;
-
-                stopwatchTimer.StopTimer();
             }
         }
 
         public void OnResetButtonClick(object sender, RoutedEventArgs e)
         {
-            stopwatchTimer.ResetTime();
+            stopwatchTimer.Reset();
             TimeLabel.Content = "00:00.00";
         }
 
-        private async void SyncTimer()
+        public void OnExtraWindowClick(object sender, RoutedEventArgs e)
         {
-            while (isRunning)
-            {
-                await Task.Delay(50);
-                TimeLabel.Content = stopwatchTimer.GetTimeString();
-            }
+            var extraWindow = new ExtraWindow();
+            extraWindow.Show();
         }
 
         public void OnBackToHomeClick(object sender, RoutedEventArgs e)
