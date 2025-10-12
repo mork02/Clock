5829a461b0d6d49109dce275843d33c4c899c8d8
KeDeX <albers.mark02@gmail.com>
2025-10-12 21:57:45 +0200

    add Interface folder and Interface

---
+++ Interfaces/ITicker.cs
@@ -0,0 +1,15 @@
+﻿namespace Clock
+{
+    interface ITicker
+    {
+        double Time { get; }
+        int Interval { get; set; }
+        bool IsRunning { get; }
+
+        string GetTimeString();
+
+        void Start();
+        void Stop();
+        void Reset();
+    }
+}
