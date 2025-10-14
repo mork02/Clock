4c2a1e7c4382d2c0a6ed901ea523adc9f91dd428
KeDeX <albers.mark02@gmail.com>
2025-10-14 10:50:42 +0200

    add logic, maxentries and a 3 second template countdown

--- WPF/CountdownPage.xaml.cs
+++ WPF/CountdownPage.xaml.cs
@@ -15,14 +15,72 @@ using System.Windows.Shapes;
 
 namespace Clock.WPF
 {
-    /// <summary>
-    /// Interaction logic for CountdownPage.xaml
-    /// </summary>
     public partial class CountdownPage : Page
     {
+        private readonly List<CountdownTicker> countdownTickers = new();
+        private int entryCount = 0;
+        private const int MaxEntries = 5;
+
         public CountdownPage()
         {
             InitializeComponent();
+        }
+
+        public void OnCreateButton(object sender, RoutedEventArgs e)
+        {
+            if (entryCount >= MaxEntries)
+            {
+                MessageBox.Show($"Maximum of {MaxEntries} entries reached.");
+                return;
+            }
+            entryCount++;
+
+            int initialTime = 3000;
+
+            CountdownTicker ticker = new CountdownTicker(initialTime);
+
+            ticker.Start();
+
+            ticker.Tick += (_, __) =>
+            {
+                if (ticker.Time >= 0)
+                {
+                    ticker.Stop();
+                }
+
+                TimerContainer.Children
+                    .OfType<TextBox>()
+                    .ElementAt(countdownTickers.IndexOf(ticker))
+                    .Text = ticker.GetTimeString();
+
+            };
+
+            countdownTickers.Add(ticker);
+
+            string text = $"{ticker.GetTimeString()}";
+
+            createTextbox(TimerContainer, text);
+
+        }
+
+        private void createTextbox(Panel parent, string text)
+        {
+            TextBox textBox = new TextBox
+            {
+                Text = text,
+                Margin = new Thickness(2),
+                IsReadOnly = true
+            };
+            if (InputDescription.Text.Length > 1)
+            {
+                TextBlock descrription = new TextBlock
+                {
+                    Text = $"{InputDescription.Text}",
+                    Margin = new Thickness(2),
+                };
+                parent.Children.Add(descrription);
+            }
+            parent.Children.Add(textBox);
         }
 
         public void OnBackToHomeClick(object sender, RoutedEventArgs e)
